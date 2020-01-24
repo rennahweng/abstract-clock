6add9e5808c15848eb55669bb2369f4e02feda2b
rennahweng <rennahweng628@gmail.com>
2020-01-24 12:52:11 -0500

    Set background color and added gradient design

--- sketch.js
+++ sketch.js
@@ -1,14 +1,39 @@
+// variables for gradient background design
+let noiseY;
+let noiseSpeed = 0.01;
+let noiseHeight = 20;
+
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	// set frame as the whole webpage based on user's screen size
+    createCanvas(windowWidth,windowHeight);
+
+    // for gradient background
+    noiseY = height * 3 / 4;
+    
+    frameRate(1);
 }
 
+
 function draw() {
-	background(225);
-	textSize(32);
-	fill(180);
-	text(hour(), 10, 30);
-	fill(100);
-	text(minute(), 10, 60);
-	fill(0);
-	text(second(), 10, 90);
+	// yellow and green-ish background color
+	background(255, 250, 195);
+	// add gradient design to the whole background
+    background_gradient();
+}
+
+function background_gradient() {
+	for (let j = 0; j < 3; j++) {
+      let offsetY = j * 100;
+      noFill();
+      stroke(0, 0, 255, 10);
+      strokeWeight(height / 2);
+      beginShape();
+      curveVertex(0, height / 2);
+      for (let i = 0; i < width; i += 50) {
+        let y = noise(frameCount * noiseSpeed + i + j) * noiseHeight + noiseY + offsetY;
+        curveVertex(i, y);
+      }
+      curveVertex(width, height / 2);
+      endShape(CLOSE);
+    }
 }
