e0332689d0535bbd8ff0207b0648fdbe494984d7
rennahweng <rennahweng628@gmail.com>
2020-01-24 17:05:58 -0500

    Make water circles to represent seconds

--- sketch.js
+++ sketch.js
@@ -25,9 +25,21 @@ function draw() {
 
     // Add blinking stars to background
     stars();
+
+    // Generate clock based on hour, minute, and second:
+    var h = hour();
+    var m = minute();
+    var s = second();
+
+    // Make circles of decreasing radius to represent second
+    circleSecond(s);
+
 }
 
 
+// Helper Functions:
+
+// for background decorations
 function background_gradient() {
 	for (let j = 0; j < 3; j++) {
       let offsetY = j * 100;
@@ -67,4 +79,42 @@ function stars() {
 	// a stream of stars following after mouse cursor
 	ellipse(mouseX ,mouseY, galaxy.size,galaxy.size);
 	ellipse(galaxy.locationX ,galaxy.locationY, galaxy.size, galaxy.size);
+}
+
+
+// Functions for Clock Display:
+
+// Seconds:
+function circleSecond(s) {
+    push();
+    translate(width/2, height/2);
+    // Update the number of circles based on second
+    for (i = 0; i < s; i++) {
+	    stroke(124, 185, 232); // light blue
+	    strokeWeight(2);
+	    noFill();
+	    // first circle has the same width as the earth: 530
+	    ellipse(0, 0, 530-i*9, 530-i*9);
+	    // widths of the subsequent circles are updated to smaller values
+    }
+    pop();
+    /** These blue circles represent water level.
+     *	When water circles fill up the whole earth,
+     *	flowers (minute) and trees (hour) will grow,
+     *	thus makes Earth greener and prettier!!
+     *	See more details in flowerMinute and treeHour
+     *	functions below.
+     **/
+    
+    // Make a water drop at the center of the earth when second restarts at 0.
+    // This shows how we are currently collecting water circles to fill up the Earth!
+    for(var j = 0; j < s; j++) {
+      for (var i = 2; i < 10; i++ ) {
+        noStroke();
+        fill(124, 185, 232); // light blue
+        // make multiple ellipses of different widths
+        // and combine them to form a water-drop shape:
+        ellipse(width/2, height/2.2 + i*4, i*2, i*2);
+      }
+    }
 }
